81f471e72d86d70f53e88f59be4ca5afeff23c20
Administrator <Administrator@MiaoXG>
2015-04-06 23:43:49 +0800

    常量类, 包含中心服务器地址

---
+++ src/main/java/com/miaoxg/device/monitor/core/Constants.java
@@ -0,0 +1,7 @@
+package com.miaoxg.device.monitor.core;
+
+public class Constants {
+    public static final String SERVER_IP = "1.93.12.250";
+    public static final int SERVER_PORT = 8081;
+    
+}
