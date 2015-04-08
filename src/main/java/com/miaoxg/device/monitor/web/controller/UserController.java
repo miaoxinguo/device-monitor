c6e7f2d9da5ed710f5f07b6de25aeb5ed22ed0c3
Administrator <Administrator@MiaoXG>
2015-04-09 00:34:44 +0800

    增加一个demo用户

--- src/main/java/com/miaoxg/device/monitor/web/controller/UserController.java
+++ src/main/java/com/miaoxg/device/monitor/web/controller/UserController.java
@@ -26,25 +26,38 @@ public class UserController extends AbstractController{
     
     @RequestMapping(value="auth", method=RequestMethod.POST)
     public String login(String username, String password, HttpSession session) {
-        logger.debug("username is {}", username);
-        logger.debug("password is {}", password);
-       
+        logger.debug("username = {}, password = {}", username, password);
+        
+        
         // TODO 暂时写死用户名/口令，以后从数据库判断
-        if(!"admin".equals(username)){
+        if(!"admin".equals(username) && !"demo".equals(username)){
              return JsonUtils.toFailureJson("用户名不存在");
         }
+        
+        // 验证密码
         MessageDigest messageDigest = null;
         try {
             messageDigest = MessageDigest.getInstance("SHA1");
         } catch (NoSuchAlgorithmException e) {
             return JsonUtils.toFailureJson("系统异常");
         }
-        messageDigest.update("admin".getBytes());
+        messageDigest.update("123456".getBytes());
+        
         String passwordHex = Hex.encodeHexString(messageDigest.digest());
         if(!passwordHex.equals(password)){
             return JsonUtils.toFailureJson("密码错误");
         }
-        session.setAttribute("user", username);
+        session.setAttribute("isAdmin", "admin".equals(username));
         return JsonUtils.toSuccessJson();
+    }
+    
+    /**
+     * 获取权限
+     */
+    @RequestMapping(value="menu", method=RequestMethod.GET)
+    public String getUserMenu(HttpSession session) {
+        // TODO 临时方式
+        boolean isAdmin = (boolean)session.getAttribute("isAdmin");
+        return JsonUtils.toSuccessJson(isAdmin ? "1" : "0");
     }
 }
