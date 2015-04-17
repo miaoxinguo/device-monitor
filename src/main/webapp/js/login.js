940f11d400337f770721f54d7a1583c930711333
Administrator <Administrator@MiaoXG>
2015-04-17 08:59:51 +0800

    增加回车事件

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,21 +1,33 @@
-$("#btnSubmit").click(function() {
-	var username = $("#inputUsername").val();
-	var password = $("#inputPassword").val();
-	
-	$.ajax({
-		url: '/device-monitor/auth',
-		type: 'POST',
-		dataType: 'json',
-		data: {
-			username: $("#inputUsername").val(),
-			password: hex_sha1($("#inputPassword").val())
-		},
-		success:function(data){
-			if(!data.success){
-				$("#errMsg").text(data.msg).show();
-				return;
+$(document).ready(function(){
+	// 事件 - 登录按钮
+	$("#btnSubmit").click(function() {
+		var username = $("#inputUsername").val();
+		var password = $("#inputPassword").val();
+		
+		$.ajax({
+			url: '/device-monitor/auth',
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				username: $("#inputUsername").val(),
+				password: hex_sha1($("#inputPassword").val())
+			},
+			success:function(data){
+				if(!data.success){
+					$("#errMsg").text(data.msg).show();
+					return;
+				}
+				window.location.href="main.html";
 			}
-			window.location.href="main.html";
-		}
+		});
 	});
+	
+	// 事件 - 回车
+	document.onkeydown = function(e){
+	    var ev = document.all ? window.event : e;
+	    if(ev.keyCode==13) {
+	           $('#btnSubmit').click();
+	     }
+	}
+	
 });
