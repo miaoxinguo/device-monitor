8158a4ad57764f354324cc98b2678761b8d2d3a3
Administrator <Administrator@MiaoXG>
2015-04-18 23:26:14 +0800

    实现用户登录、访问控制以及用户信息查看方法

--- src/main/java/com/miaoxg/device/monitor/service/UserService.java
+++ src/main/java/com/miaoxg/device/monitor/service/UserService.java
@@ -1,9 +1,46 @@
 package com.miaoxg.device.monitor.service;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.miaoxg.device.monitor.core.ServiceException;
+import com.miaoxg.device.monitor.dao.UserDao;
+import com.miaoxg.device.monitor.entity.User;
 
 @Service
+@Transactional
 public class UserService {
+    @Resource
+    private UserDao userDao;
     
-   
+    /**
+     * 分页查询符合记录的用户、符合条件的用户总数
+     */
+    public Object[] getListAndTotalCount(Map<String, Object> param){
+        int count = userDao.selectCount(param);                 // 符合条件的记录总数
+        List<User> userList = userDao.selectList(param);  
+        return new Object[]{count, userList};
+    }
+    
+    /**
+     * 用户登录
+     */
+    public User login(String name, String password){
+        User user = userDao.selectUserByName(name);
+        if(user == null){
+            throw new ServiceException("用户："+name+" 不存在");
+        }
+        
+        // 验证密码
+        if(!password.equals(user.getPassword())){
+            throw new ServiceException("密码错误");
+        }
+        user.setPassword(""); // user会存入session，不保存密码
+        return user;
+    }
 }
--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 	$("#userTable").dataTable({
 		"bPaginate": true,
 	    "bProcessing": true,                    //加载数据时显示正在加载信息   
-	    //"bServerSide": true,                    //指定从服务器端获取数据   
-	    //"sAjaxSource": "users",	//获取数据的url   
-	    "aoColumns": [   {"mDataProp":"id"},  
-	                     {"mDataProp":"name"},
-	                     {"mDataProp":"role"},
-	                     {"mDataProp":"hotel"}],  // 与后台返回属性一致
-	    //"bFilter": false,                       //不使用过滤功能     
+	    "bServerSide": true,                    //指定从服务器端获取数据   
+	    "sAjaxSource": "users",	//获取数据的url   
+	    "aoColumns": [{"mDataProp":"id"},
+	                  {"mDataProp":"name"},
+	                  {"mDataProp":"role"},
+                      {"mDataProp":"roleName"}],  // 与后台返回属性一致
+//	    "bFilter": false,                       //不使用过滤功能     
 	    "sPaginationType": "full_numbers",      //翻页界面类型   
 	    "oLanguage": {    
 	    	"sUrl": "media/i18n/zh_CN.txt"
