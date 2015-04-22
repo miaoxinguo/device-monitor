98c01bd21442a79810bd1821c28451da8b6276c3
Administrator <Administrator@MiaoXG>
2015-04-22 17:26:13 +0800

    增加查询设备信息的方法

--- src/main/java/com/miaoxg/device/monitor/dao/DeviceDao.java
+++ src/main/java/com/miaoxg/device/monitor/dao/DeviceDao.java
@@ -8,16 +8,68 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.miaoxg.device.monitor.entity.MonitorValue;
+import com.miaoxg.device.monitor.vo.MonitorValueVo;
 
 @Repository
 public class DeviceDao extends BaseDao {
     
     /**
-     * 查询所有的设备编号
+     * 分页查询某酒店的所有设备, 如果没有指定酒店id, 抛异常
+     */
+    public List<String> selectDeviceSidByHotel(MonitorValueVo vo){
+        String sql = "select d.sid from device d where d.hotel_id = ? limit ?, ?";
+        Object[] params = {vo.getHotelId(), vo.getiDisplayStart(), vo.getiDisplayLength()};
+        return getJdbcTemplate().queryForList(sql, String.class, params);
+    }
+    
+    /**
+     * 查询某酒店的所有设备数量
+     */
+    public int selectDeviceCountByHotel(MonitorValueVo vo){
+        String sql = "select count(*) from device d where d.hotel_id = ? ";
+        return getJdbcTemplate().queryForObject(sql, Integer.class, vo.getHotelId());
+    }
+    
+    /**
+     * 查询某用户（不含管理员）所有酒店所有设备的编号
+     */
+    public List<String> selectDeviceSidByUser(MonitorValueVo vo){
+        String sql = "SELECT d.sid from device d "
+                + "left join hotel h on d.hotel_id=h.id "
+                + "left join user_hotel on h.id=user_hotel.hotel_id "
+                + "left join user on user_hotel.user_id=user.id "
+                + "where user_id=? limit ?, ?";
+        Object[] params = {vo.getUserId(), vo.getiDisplayStart(), vo.getiDisplayLength()};
+        return getJdbcTemplate().queryForList(sql, String.class, params);
+    }
+    
+    /**
+     * 查询某用户（不含管理员）所有酒店所有设备的数量
+     */
+    public int selectDeviceCountByUser(MonitorValueVo vo){
+        String sql = "SELECT count(*) from device d "
+                + "left join hotel h on d.hotel_id=h.id "
+                + "left join user_hotel on h.id=user_hotel.hotel_id "
+                + "left join user on user_hotel.user_id=user.id "
+                + "where user_id=? ";
+        return getJdbcTemplate().queryForObject(sql, Integer.class, vo.getUserId());
+    }
+    
+    /**
+     * 查询所有酒店所有设备的编号
      */
     public List<String> selectAllDeviceSid(){
-        String sql = "select sid from device";
+        String sql = "select d.sid from device d";
         return getJdbcTemplate().queryForList(sql, String.class);
+    }
+    
+    /**
+     * 分页查询所有酒店所有设备的编号
+     */
+    public List<String> selectAllDeviceSid(MonitorValueVo vo){
+        String sql = "select d.sid from device d limit ?,?";
+        Object[] params = {vo.getiDisplayStart(), vo.getiDisplayLength()};
+        return getJdbcTemplate().queryForList(sql, String.class, params);
     }
     
     /**
