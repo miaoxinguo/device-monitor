63afeaf6d0482a6311483d15b42ba7a396d834bb
Administrator <Administrator@MiaoXG>
2015-04-22 17:27:56 +0800

    删除测试代码，改为实际的业务逻辑

--- src/main/java/com/miaoxg/device/monitor/web/controller/DeviceController.java
+++ src/main/java/com/miaoxg/device/monitor/web/controller/DeviceController.java
@@ -1,30 +1,23 @@
 package com.miaoxg.device.monitor.web.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSONObject;
+import com.miaoxg.device.monitor.entity.MonitorValue;
+import com.miaoxg.device.monitor.entity.User;
 import com.miaoxg.device.monitor.service.DeviceService;
+import com.miaoxg.device.monitor.util.JsonUtils;
+import com.miaoxg.device.monitor.vo.DataTablesVo;
+import com.miaoxg.device.monitor.vo.MonitorValueVo;
 
 @RestController
 public class DeviceController extends AbstractController{
-    
-    // TODO 写死几个设备编号，两个数组对应两个酒店，以后从数据库取
-    private static String[] devices1 = {
-        "380058590147", "380058631466", "380058686739", "380058725928"
-    };
-    private static String[] devices2 = {
-        "380057701067", "117644925998"
-    };
     
     private static final Logger logger = LoggerFactory.getLogger(DeviceController.class);
     
@@ -33,32 +26,23 @@ public class DeviceController extends AbstractController{
     
     /**
      * 获取某个酒店的所有设备数值
-     * @param hotelId 酒店id
+     * 
+     * @param hotelId 酒店id，如果为0，查询该用户关联的所有酒店
      */
-    @RequestMapping(value="deviceStatus", method=RequestMethod.GET)
-    public String getDeviceStatus(String hotelId, String sEcho){
+    @RequestMapping(value="monitorValues", method=RequestMethod.GET)
+    public String getMonitorValues(MonitorValueVo vo, HttpSession session){
+        Integer hotelId = vo.getHotelId();
+        logger.debug("into DeviceController.monitorValues， hotelId = {}", hotelId);
+        logger.debug("offset:{}, limit:{}", vo.getiDisplayStart(), vo.getiDisplayLength());
         
-        logger.debug("hotelId = {}", hotelId);
         if(null==hotelId || "".equals(hotelId)){
-            hotelId = "0";
+            hotelId = Integer.valueOf("0");
         }
-        List<String> devList = null;
-        if("0".equals(hotelId)){  //查全部
-            devList = Arrays.asList(ArrayUtils.addAll(devices1, devices2));
-        }
-        else if("1".equals(hotelId)){
-            devList = Arrays.asList(devices1);
-        }
-        else if("2".equals(hotelId)){
-            devList = Arrays.asList(devices2);
-        }
-        
-        JSONObject obj = new JSONObject();
-        obj.put("iTotalRecords", devList.size());   // 数据库表中记录数
-        obj.put("iTotalDisplayRecords", devList.size());   // 查询条件顾虑后的记录数
-        obj.put("sEcho", sEcho);
-        obj.put("aaData", "");
-        return obj.toJSONString();
+        if(hotelId == 0){  //查当前用户所有酒店的全部设备监测值
+            vo.setUserId(((User)session.getAttribute("user")).getId());
+        } 
+        DataTablesVo<MonitorValue> dtvo = deviceService.getMonitorValue(vo);
+        return JsonUtils.toDatatablesJson(dtvo.getCount(), dtvo.getTotal(), vo.getsEcho(), dtvo.getList());
     }
     
     /**
