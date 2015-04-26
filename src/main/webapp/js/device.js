104945aa7d623ed5d6dd85f7b4338a4d908a5803
Administrator <Administrator@MiaoXG>
2015-04-26 23:40:48 +0800

    加载数据时不显示表格的遮罩层

--- src/main/webapp/js/device.js
+++ src/main/webapp/js/device.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		"bPaginate": true,
 		"bLengthChange": false, 			//改变每页显示数据数量
 		'iDisplayLength': 10, 				//每页显示记录数
-	    "bProcessing": true,                    //加载数据时显示正在加载信息   
+	    "bProcessing": false,                    //加载数据时显示正在加载信息   
 	    "bServerSide": true,                    //指定从服务器端获取数据   
 	    "sAjaxSource": "devices",				//获取数据的url   
 	    "fnServerData": function (sSource, aoData, fnCallback) {
