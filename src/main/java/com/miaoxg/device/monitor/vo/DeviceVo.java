84d77c03898a47ef458be965f6cf7cc2a27ad340
Administrator <Administrator@MiaoXG>
2015-04-28 15:52:56 +0800

    增加查询条件

--- src/main/java/com/miaoxg/device/monitor/vo/DeviceVo.java
+++ src/main/java/com/miaoxg/device/monitor/vo/DeviceVo.java
@@ -7,13 +7,25 @@ package com.miaoxg.device.monitor.vo;
  */
 public class DeviceVo extends Page{
     private Integer userId;
+    private Integer hotelId;
+    private String room;
 
     public Integer getUserId() {
         return userId;
     }
-
     public void setUserId(Integer userId) {
         this.userId = userId;
     }
-
+    public Integer getHotelId() {
+        return hotelId;
+    }
+    public void setHotelId(Integer hotelId) {
+        this.hotelId = hotelId;
+    }
+    public String getRoom() {
+        return room;
+    }
+    public void setRoom(String room) {
+        this.room = room;
+    }
 }
