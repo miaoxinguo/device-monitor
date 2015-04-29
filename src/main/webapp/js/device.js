067126fa1ccb22288bfb95ac10b2b4359fe953d3
Administrator <Administrator@MiaoXG>
2015-04-29 22:50:54 +0800

    调整页面布局

--- src/main/webapp/js/device.js
+++ src/main/webapp/js/device.js
@@ -1,6 +1,8 @@
+var t;
+
 $(document).ready(function(){
 	// 表格
-	var t = $("#deviceTable").dataTable({
+	t = $("#deviceTable").dataTable({
 		"bPaginate": true,
 		"bLengthChange": false, 			//改变每页显示数据数量
 		'iDisplayLength': 10, 				//每页显示记录数
@@ -27,8 +29,11 @@ $(document).ready(function(){
 	                     {"mData":"name"},
 	                     {"mData":"hotel.name"},
 	                     {"mData":"room"},
-	                     {"mData":"lastDate"},
-	                     {"mData":"nextDate"}],  // 与后台返回属性一致
+	                     {"mData":"usedHours", "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+	                    	 if(sData > 1500){
+	                    		 $(nTd).addClass("text-danger");
+	                    	 }
+			             }}],  // 与后台返回属性一致
 	    "bFilter": false,                       //不使用过滤功能     
 	    "sPaginationType": "full_numbers",      //翻页界面类型   
 	    "oLanguage": {                          //国际化   
@@ -47,129 +52,137 @@ $(document).ready(function(){
 	});
 	 
 	/*
-	 * 增加按钮点击事件
+	 * 加载酒店下拉框内容
 	 */ 
-	$("#btn_add").click(function(){
-		$("#form_add #form_add_sid").removeAttr("readonly");
-		$("#form_add").show();
-		$("#content_list").hide();
+	$.get("hotelNames", function(data){
 		
-		// 加载酒店下拉框
-		$.get("hotelNames", function(data){
-			var $hotelSelect = $("#select_hotel");
-			for(var index in data){
-				$hotelSelect.append("<option value='" + data[index].id + "'>"+data[index].name+"</option>");
-			}
-		});
+		// 查询区下拉框
+		var $search_hotelSelect = $("#hotelList");
 		
-		// 保存按钮
-		$("#btn_save").click(function(){
-			$("#form_add form").ajaxSubmit({
-				type: "post",
-				url: "device",
-			    dataType: "json",
-				success: function(data){
-					if(!data.success){
-						alert(data.msg);
-						return;
-					}
-					$("#form_add form").clearForm();
-					$("#deviceTable").DataTable().draw();   // 取表格对象 刷新
-					alert("保存成功");
-				}
-			});
-			return false;
-		});
+		// form区下拉框
+		var $form_hotelSelect = $("#select_hotel");
 		
-		// 取消按钮
-		$("#btn_cancel").click(function(){
-			$("#content_list").show();
-			$("#form_add").hide();
-			$("#form_add").clearForm();
-		});
-	});
-	
-	/*
-	 * 删除按钮点击事件
-	 */
-	$("#btn_remove").click(function(){
-		if (!$("#deviceTable tbody tr").hasClass('selected')) {
-			alert("请选择一行");
-			return;
+		for(var index in data){
+			var option = "<option value='"+ data[index].id +"'>"+ data[index].name +"</option>";
+			$search_hotelSelect.append(option);
+			$form_hotelSelect.append(option);
 		}
-		
-		// 提交后台删除
-		$.ajax({
-			type: 'delete',
-			url: "device/" + t.$('tr.selected').children().eq(0).text(),
-			dataType: 'json',
-			success:function(data){
-				alert("删除成功");
-				$("#deviceTable").DataTable().draw();   // 取表格对象 刷新
-			}	
-		});
 	});
-	
-	/*
-	 * 编辑按钮点击事件
-	 */
-	$("#btn_edit").click(function(){
-		if (!$("#deviceTable tbody tr").hasClass('selected')) {
-			alert("请选择一行");
-			return;
-		}
-		
-		// 加载酒店下拉框
-		$.get("hotelNames", function(data){
-			var $hotelSelect = $("#select_hotel");
-			for(var index in data){
-				$hotelSelect.append("<option value='" + data[index].id + "'>"+data[index].name+"</option>");
-			}
-			
-			// 从后台读数据
-			$.ajax({
-				type: 'get',
-				url: "device?sid=" + t.$('tr.selected').children().eq(0).text(),
-				dataType: 'json',
-				success:function(data){
-					$("#form_add #form_add_sid").val(data.sid);
-					$("#form_add #select_hotel").children("option[value='"+data.hotel.id+"']").attr("selected",true);
-					$("#form_add #form_add_room").val(data.room);
-					$("#form_add #form_add_last_date").val(data.lastDate);
-					$("#form_add #form_add_next_date").val(data.nextDate);
-				}	
-			});
-		});
-		
-		$("#form_add #form_add_sid").attr("readonly","readonly");
-		$("#form_add").show();
-		$("#content_list").hide();
-		
-		// 保存按钮
-		$("#btn_save").click(function(){
-			$("#form_add form").ajaxSubmit({
-				type: "put",
-				url: "device",
-			    dataType: "json",
-				success: function(data){
-					if(!data.success){
-						alert(data.msg);
-						return;
-					}
-					alert("保存成功");
-					$("#deviceTable").DataTable().draw();   // 取表格对象 刷新
-					$("#btn_cancel").click();
-					$("#form_add form").clearForm();
-				}
-			});
-			return false;
-		});
-		
-		// 取消按钮
-		$("#btn_cancel").click(function(){
-			$("#content_list").show();
-			$("#form_add").hide();
-			$("#form_add").clearForm();
-		});
-	})
 });
+
+//事件 - 酒店下拉选择
+$("#search_div #hotelList").change(function(){
+	$("#deviceTable").DataTable().draw();   // 取表格对象 刷新
+});
+
+//事件 - 酒店模糊查询
+$("#search_div #room").keyup(function(){
+	$("#deviceTable").DataTable().draw();   // 取表格对象 刷新
+});
+
+/*
+ * 增加按钮点击事件
+ */ 
+$("#btn_add").click(function(){
+	$("#form_add #form_add_sid").removeAttr("readonly");
+	$("#form_add").show();
+	$("#device_content").hide();
+	
+	// 保存
+	$("#btn_save").click(function(){
+		$("#form_add form").ajaxSubmit({
+			type: "post",
+			url: "device",
+		    dataType: "json",
+			success: function(data){
+				if(!data.success){
+					alert(data.msg);
+					return;
+				}
+				$("#form_add form").clearForm();
+				$("#deviceTable").DataTable().draw();   // 取表格对象 刷新
+				alert("保存成功");
+			}
+		});
+		return false;
+	});
+	
+	// 取消
+	$("#btn_cancel").click(function(){
+		$("#device_content").show();
+		$("#form_add").hide().clearForm().find("button").unbind();   // 也可以改为页面加载后绑定，这里就不用解除了
+	});
+});
+
+/*
+ * 删除按钮点击事件
+ */
+$("#btn_remove").click(function(){
+	if (!$("#deviceTable tbody tr").hasClass('selected')) {
+		alert("请选择一行");
+		return;
+	}
+	
+	// 提交后台删除
+	$.ajax({
+		type: 'delete',
+		url: "device/" + t.$('tr.selected').children().eq(0).text(),
+		dataType: 'json',
+		success:function(data){
+			alert("删除成功");
+			$("#deviceTable").DataTable().draw();   // 取表格对象 刷新
+		}	
+	});
+});
+
+/*
+ * 编辑按钮点击事件
+ */
+$("#btn_edit").click(function(){
+	if (!$("#deviceTable tbody tr").hasClass('selected')) {
+		alert("请选择一行");
+		return;
+	}
+	
+	// 从后台读数据
+	$.ajax({
+		type: 'get',
+		url: "device?sid=" + t.$('tr.selected').children().eq(0).text(),
+		dataType: 'json',
+		success:function(data){
+			$("#form_add #form_add_sid").val(data.sid);
+			$("#form_add #select_hotel").children("option[value='"+data.hotel.id+"']").attr("selected",true);
+			$("#form_add #form_add_room").val(data.room);
+		}	
+	});
+	
+	$("#form_add #form_add_sid").attr("readonly","readonly");
+	$("#form_add").show();
+	$("#device_content").hide();
+	
+	// 保存
+	$("#btn_save").click(function(){
+		$("#form_add form").ajaxSubmit({
+			type: "put",
+			url: "device",
+		    dataType: "json",
+			success: function(data){
+				if(!data.success){
+					alert(data.msg);
+					return;
+				}
+				alert("保存成功");
+				$("#deviceTable").DataTable().draw();   // 取表格对象 刷新
+				$("#btn_cancel").click();
+				$("#form_add form").clearForm();
+			}
+		});
+		return false;
+	});
+	
+	// 取消
+	$("#btn_cancel").click(function(){
+		$("#device_content").show();
+		$("#form_add").hide().clearForm().find("button").unbind();
+	});
+})
