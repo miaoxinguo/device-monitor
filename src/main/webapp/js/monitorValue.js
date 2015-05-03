d9e3ac48502fb4a0ea69923112721ee2e4575f34
Administrator <Administrator@MiaoXG>
2015-05-04 00:32:12 +0800

    增加演示图表

--- src/main/webapp/js/monitorValue.js
+++ src/main/webapp/js/monitorValue.js
@@ -158,6 +158,88 @@ $('#monitorValueTable').DataTable().on( 'draw', function () {
 	        		$("#edit_modal #form_name").val($tr.children("td:eq(1)").text());
 	        		$("#edit_modal").modal(); 
 	        	})
+	        	
+	        	// 图标点击事件
+	        	$modalDiv.find("div.panel-footer a:eq(2)").click(function(){
+	        		$("#report_modal").modal(); 
+			        // 基于准备好的dom，初始化echarts图表
+			        var myChart = echarts.init(document.getElementById('report_div')); 
+			        
+			        option = {
+			                title : {
+			                    text: '温度变化',
+			                    subtext: '演示'
+			                },
+			                tooltip : {
+			                    trigger: 'axis'
+			                },
+			                legend: {
+			                    data:['最高温度','最低温度']
+			                },
+			                toolbox: {
+			                    show : true,
+			                    feature : {
+			                       // mark : {show: true},
+			                       // dataView : {show: true, readOnly: false},
+			                        //magicType : {show: true, type: ['line', 'bar']},
+			                        restore : {show: true},
+			                        saveAsImage : {show: true}
+			                    }
+			                },
+			                calculable : true,
+			                xAxis : [
+			                    {
+			                        type : 'category',
+			                        boundaryGap : false,
+			                        data : ['周一','周二','周三','周四','周五','周六','周日']
+			                    }
+			                ],
+			                yAxis : [
+			                    {
+			                        type : 'value',
+			                        axisLabel : {
+			                            formatter: '{value} °C'
+			                        }
+			                    }
+			                ],
+			                series : [
+			                    {
+			                        name:'最高温度',
+			                        type:'line',
+			                        data:[25, 26, 25, 22, 25, 24, 25],
+			                        markPoint : {
+			                            data : [
+			                                {type : 'max', name: '最大值'},
+			                                {type : 'min', name: '最小值'}
+			                            ]
+			                        },
+			                        markLine : {
+			                            data : [
+			                                {type : 'average', name: '平均值'}
+			                            ]
+			                        }
+			                    },
+			                    {
+			                        name:'最低气温',
+			                        type:'line',
+			                        data:[21, 25, 24, 21, 22, 21, 25],
+			                        markPoint : {
+			                            data : [
+			                                {name : '周最低', value : -2, xAxis: 1, yAxis: -1.5}
+			                            ]
+			                        },
+			                        markLine : {
+			                            data : [
+			                                {type : 'average', name : '平均值'}
+			                            ]
+			                        }
+			                    }
+			                ]
+			            };
+			
+			        // 为echarts对象加载数据 
+			        myChart.setOption(option); 
+	        	})
 	        }
 	    });
 	}
